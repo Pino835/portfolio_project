1d3dea54dafb56f4cb45aba85fd583e66df395ac
Juan Pino <pinoto2002@gmail.com>
2025-10-14 11:04:17 -0600

    Commit #16: - Cambios. - Redireccion a mypage.

--- mypage/core/views.py
+++ mypage/core/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Project
+
+def redirect_to_mypage(request):
+    return redirect('/mypage/')
 
 def home(request):
     return render(request, 'core/home.html')
--- mypage/mypage/urls.py
+++ mypage/mypage/urls.py
@@ -3,8 +3,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from core import views
 
 urlpatterns = [
+    path('', views.redirect_to_mypage),
     path('admin/', admin.site.urls),
     path('mypage/', include('core.urls')),
 ]
